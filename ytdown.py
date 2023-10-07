2849c7c802208725d03d5a4e3debb99bacf277f5
aaronjc15128 <aaronjchauhan2@gmail.com>
2023-10-07 15:36:35 +0100

    Added main content

--- ytdown.py
+++ ytdown.py
@@ -1,5 +1,43 @@
-def main():
-    pass
+from pytube import YouTube
+from os import system
+import time
+import math
 
-if __name__ == "__main__":
-    main()
+SAVE_PATH = "downloads"
+
+links = []
+links_file = open("links.txt", "r")
+for link in links_file:
+    links.append(link)
+links_file.close()
+
+start_time = time.time()
+
+def message(title, number):
+    current_time = time.time()
+    time_elapsed = current_time - start_time
+    time_elapsed_minutes = math.floor(time_elapsed/60)
+    time_elapsed_seconds = math.floor(time_elapsed-(time_elapsed_minutes*60))
+    
+    system("clear")
+    print(f"{number} / {len(links)}   {round(number/len(links)*100)}%    {time_elapsed_minutes}m {time_elapsed_seconds}s    {title}")
+
+i = 0
+for link in links:
+    try:
+        video = YouTube(link).streams.get_highest_resolution()
+    except:
+        with open("errors.txt", "w") as errors:
+            errors.write(f"C:    {video.title}")
+
+    message(video.title, i)
+
+    try:
+        video.download(SAVE_PATH)
+    except:
+        with open("errors.txt", "w") as errors:
+            errors.write(f"D:    {video.title}")
+
+    i += 1
+
+message("Done!", i)
