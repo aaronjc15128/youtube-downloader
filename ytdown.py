5948df06da50fc60124f79ec22ee8b8cd176045c
aaronjc15128 <aaronjchauhan2@gmail.com>
2023-10-07 16:22:22 +0100

    Added some features

--- ytdown.py
+++ ytdown.py
@@ -18,9 +18,17 @@ def message(title, number):
     time_elapsed = current_time - start_time
     time_elapsed_minutes = math.floor(time_elapsed/60)
     time_elapsed_seconds = math.floor(time_elapsed-(time_elapsed_minutes*60))
+    if number == 0:
+        time_eta_minutes = time_eta_seconds = "."
+    elif number == len(links):
+        time_eta_minutes = time_eta_seconds = "0"
+    else:
+        time_eta = ((time_elapsed/number)*len(links))-time_elapsed
+        time_eta_minutes = math.floor(time_eta/60)
+        time_eta_seconds = math.floor(time_eta-(time_eta_minutes*60))
     
     system("clear")
-    print(f"{number} / {len(links)}   {round(number/len(links)*100)}%    {time_elapsed_minutes}m {time_elapsed_seconds}s    {title}")
+    print(f"{number}/{len(links)}    {round(number/len(links)*100)}%    {time_elapsed_minutes}m {time_elapsed_seconds}s    ETA {time_eta_minutes}m {time_eta_seconds}s    {title}")
 
 i = 0
 for link in links:
