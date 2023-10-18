d94348f87d2b696d1b36a92929bfd86b932bf38e
Aaron Chauhan <aaronjchauhan2@gmail.com>
2023-10-18 17:29:23 +0100

    Added operating system option for system clear terminal command

--- ytdown.py
+++ ytdown.py
@@ -1,51 +1,63 @@
-from pytube import YouTube
-from os import system
-import time
-import math
+def main():
+    from pytube import YouTube
+    from os import system
+    import sys
+    import time
+    import math
 
-SAVE_PATH = "downloads"
+    SAVE_PATH = "downloads"
 
-links = []
-links_file = open("links.txt", "r")
-for link in links_file:
-    links.append(link)
-links_file.close()
+    links = []
+    links_file = open("links.txt", "r")
+    for link in links_file:
+        links.append(link)
+    links_file.close()
 
-start_time = time.time()
+    opsys = input("Operating System (L/W):   ").lower()
+    if opsys not in {"l", "w"}:
+        print("Invalid!")
+        sys.exit()
 
-def message(title, number):
-    current_time = time.time()
-    time_elapsed = current_time - start_time
-    time_elapsed_minutes = math.floor(time_elapsed/60)
-    time_elapsed_seconds = math.floor(time_elapsed-(time_elapsed_minutes*60))
-    if number == 0:
-        time_eta_minutes = time_eta_seconds = "."
-    elif number == len(links):
-        time_eta_minutes = time_eta_seconds = "0"
-    else:
-        time_eta = ((time_elapsed/number)*len(links))-time_elapsed
-        time_eta_minutes = math.floor(time_eta/60)
-        time_eta_seconds = math.floor(time_eta-(time_eta_minutes*60))
-    
-    system("clear")
-    print(f"{number}/{len(links)}    {round(number/len(links)*100)}%    {time_elapsed_minutes}m {time_elapsed_seconds}s    ETA {time_eta_minutes}m {time_eta_seconds}s    {title}")
+    start_time = time.time()
 
-i = 0
-for link in links:
-    try:
-        video = YouTube(link).streams.get_highest_resolution()
-    except:
-        with open("errors.txt", "w") as errors:
-            errors.write(f"C:    {video.title}")
+    def message(title, number):
+        current_time = time.time()
+        time_elapsed = current_time - start_time
+        time_elapsed_minutes = math.floor(time_elapsed/60)
+        time_elapsed_seconds = math.floor(time_elapsed-(time_elapsed_minutes*60))
+        if number == 0:
+            time_eta_minutes = time_eta_seconds = "."
+        elif number == len(links):
+            time_eta_minutes = time_eta_seconds = "0"
+        else:
+            time_eta = ((time_elapsed/number)*len(links))-time_elapsed
+            time_eta_minutes = math.floor(time_eta/60)
+            time_eta_seconds = math.floor(time_eta-(time_eta_minutes*60))
 
-    message(video.title, i)
+        if opsys == "l": system("clear")
+        elif opsys == "w": system("cls")
 
-    try:
-        video.download(SAVE_PATH)
-    except:
-        with open("errors.txt", "w") as errors:
-            errors.write(f"D:    {video.title}")
+        print(f"{number}/{len(links)}    {round(number/len(links)*100)}%    {time_elapsed_minutes}m {time_elapsed_seconds}s    ETA {time_eta_minutes}m {time_eta_seconds}s    {title}")
 
-    i += 1
+    i = 0
+    for link in links:
+        try:
+            video = YouTube(link).streams.get_highest_resolution()
+        except:
+            with open("errors.txt", "w") as errors:
+                errors.write(f"C:    {video.title}")
 
-message("Done!", i)
+        message(video.title, i)
+
+        try:
+            video.download(SAVE_PATH)
+        except:
+            with open("errors.txt", "w") as errors:
+                errors.write(f"D:    {video.title}")
+
+        i += 1
+
+    message("Done!", i)
+
+if __name__ == "__main__":
+    main()
